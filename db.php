8ca302ded90ded779c10ecc17fb87362cb33dc2e
xandybandy <xanderfernandes119@gmail.com>
2026-03-16 21:16:44 +0530

    changed pass

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("database-1.c9igmcsmmcld.ap-south-1.rds.amazonaws.com", "admin", "kingxande08", "kayaking");
+$conn = mysqli_connect("database-1.c9igmcsmmcld.ap-south-1.rds.amazonaws.com", "admin", "xandy08", "kayaking");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
